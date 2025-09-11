e24b59dc42e748d71641e2c214ef9807c64dd5de
Ravena Sweet <ggustavo.ofc@gmail.com>
2025-09-11 12:11:44 -0300

    página login e views relacionadas a login e logout

--- neurotriagem/user/urls.py
+++ neurotriagem/user/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import register_view
+from .views import register_view,CustomLoginView, CustomLogoutView
 
 urlpatterns = [
     path('registro/', register_view, name='registro'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
 ]
--- neurotriagem/user/views.py
+++ neurotriagem/user/views.py
@@ -1,9 +1,9 @@
-# user/views.py
-
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import login
 from .forms import RegistroUsuarioForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
 
 def register_view(request):
     if request.method == 'POST':
@@ -12,9 +12,28 @@ def register_view(request):
             user = form.save()
             messages.success(request, 'Registro realizado com sucesso!')
             login(request, user)
-            return redirect('home')  # Substitua por sua URL inicial
+            return redirect('index') 
         else:
             messages.error(request, 'Corrija os erros abaixo.')
     else:
         form = RegistroUsuarioForm()
     return render(request, 'user/registro.html', {'form': form})
+
+
+class CustomLoginView(LoginView):
+    template_name = 'user/login.html'
+
+    def get_success_url(self):
+        user = self.request.user
+        if user.tipo == 'normal':
+            return reverse_lazy('index')
+        elif user.tipo == 'psicologo':
+            return reverse_lazy('index')
+        elif user.tipo == 'admin':
+            return reverse_lazy('index') 
+        return reverse_lazy('home')
+
+
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy('login')
+
